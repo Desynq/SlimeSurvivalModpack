346fc180618a13088bf12e5c27a349c00f45e4be
Desynq <notdesynq@gmail.com>
2025-09-03 15:25:48 -0400

    Implement jump key event handling in client script

--- kubejs/client_scripts/main.js
+++ kubejs/client_scripts/main.js
@@ -1,3 +1,21 @@
-// Visit the wiki for more info - https://kubejs.com/
-console.info('Hello, World! (Loaded client example script)')
+let $CompoundTag = Java.loadClass("net.minecraft.nbt.CompoundTag");
+let $Minecraft = Java.loadClass("net.minecraft.client.Minecraft");
+let $ScreenEvent$KeyPressed$Pre = Java.loadClass("net.neoforged.neoforge.client.event.ScreenEvent$KeyPressed$Pre");
 
+
+let wasJumpDown = false;
+
+ClientEvents.tick(event => {
+	const isJumpDown = $Minecraft.instance.options.keyJump.down;
+	let justPressedJumpDown = false;
+
+	if (isJumpDown && !wasJumpDown) {
+		// Jump key was just pressed
+		justPressedJumpDown = true;
+		const payload = new $CompoundTag();
+		payload.putString("key", "jump");
+		Client.player.sendData("KeyPressed", payload);
+	}
+
+	wasJumpDown = isJumpDown;
+});
