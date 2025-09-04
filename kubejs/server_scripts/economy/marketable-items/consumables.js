d9f1d74156fecc0d5ef396b98ea50f7da9324eeb
Desynq <notdesynq@gmail.com>
2025-09-04 19:53:06 -0400

    Arrows can be bought now.

--- kubejs/server_scripts/economy/marketable-items/consumables.js
+++ kubejs/server_scripts/economy/marketable-items/consumables.js
@@ -1,3 +1,7 @@
 new MarketableItem("experience_bottle", "minecraft:experience_bottle")
 	.setBuyPrice(25.00)
 	.register();
+
+new MarketableItem("arrow", "minecraft:arrow")
+	.setBuyPrice(3.00)
+	.register();
