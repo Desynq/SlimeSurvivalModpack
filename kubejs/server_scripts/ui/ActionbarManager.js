17c241daff58abfd7e79d790a672ea3f3ca4d31b
Desynq <notdesynq@gmail.com>
2025-09-04 03:34:20 -0400

    Fix ActionbarManager to format action bar texts with spaces between.

--- kubejs/server_scripts/ui/ActionbarManager.js
+++ kubejs/server_scripts/ui/ActionbarManager.js
@@ -13,7 +13,7 @@ PlayerEvents.tick(event => {
 		return;
 	}
 
-	let concatText = texts.join(",");
+	let concatText = texts.join(",\" \",");
 
 	if (concatText.length > 0) {
 		server.runCommandSilent(`title ${player.username} actionbar [${concatText}]`);
