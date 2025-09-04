89f363f51626eddb5db1b8e74e25771cd34cd7df
Desynq <notdesynq@gmail.com>
2025-09-04 10:08:09 -0400

    Implement crawl key handling and food exhaustion mechanics for crawling.

--- kubejs/client_scripts/main.js
+++ kubejs/client_scripts/main.js
@@ -2,20 +2,40 @@ let $CompoundTag = Java.loadClass("net.minecraft.nbt.CompoundTag");
 let $Minecraft = Java.loadClass("net.minecraft.client.Minecraft");
 let $ScreenEvent$KeyPressed$Pre = Java.loadClass("net.neoforged.neoforge.client.event.ScreenEvent$KeyPressed$Pre");
 
+let $CrawlClient = Java.loadClass("ru.fewizz.crawl.client.CrawlClient")
 
-let wasJumpDown = false;
+
 
 ClientEvents.tick(event => {
+	JumpKeyPress();
+	CrawlKeyPress();
+});
+
+let wasJumpDown = false;
+function JumpKeyPress() {
 	const isJumpDown = $Minecraft.instance.options.keyJump.down;
-	let justPressedJumpDown = false;
 
 	if (isJumpDown && !wasJumpDown) {
-		// Jump key was just pressed
-		justPressedJumpDown = true;
 		const payload = new $CompoundTag();
 		payload.putString("key", "jump");
 		Client.player.sendData("KeyPressed", payload);
 	}
 
 	wasJumpDown = isJumpDown;
-});
+}
+
+
+let wasCrawlDown = false;
+function CrawlKeyPress() {
+	/** @type {$KeyMapping_} */
+	const key = $CrawlClient.key;
+	const isCrawlDown = key.down;
+
+	if (isCrawlDown && !wasCrawlDown) {
+		const payload = new $CompoundTag();
+		payload.putString("key", "crawl");
+		Client.player.sendData("KeyPressed", payload);
+	}
+
+	wasCrawlDown = isCrawlDown;
+}
---
+++ kubejs/server_scripts/gameplay/crawlBalancing.js
@@ -0,0 +1,33 @@
+let $Stats = Java.loadClass("net.minecraft.stats.Stats");
+let $Pose = Java.loadClass("net.minecraft.world.entity.Pose");
+
+
+NetworkEvents.dataReceived("KeyPressed", event => {
+	const player = event.player;
+	const uuid = player.uuid.toString();
+	const key = event.data.getString("key");
+
+	if (key != "crawl") {
+		return;
+	}
+
+	player.causeFoodExhaustion(4);
+});
+
+/** @type {Object.<string, integer>} */
+const prevWalkCmMap = {};
+
+PlayerEvents.tick(event => {
+	const { player } = event;
+	const uuid = player.uuid.toString();
+
+	const currWalkCm = player.stats.get($Stats.WALK_ONE_CM);
+	const prevWalkCm = prevWalkCmMap[uuid] ?? currWalkCm;
+	prevWalkCmMap[uuid] = currWalkCm;
+
+	const deltaWalkCm = currWalkCm - prevWalkCm;
+
+	if (player.pose.equals($Pose.CRAWLING)) {
+		player.causeFoodExhaustion(deltaWalkCm * 0.005);
+	}
+});
