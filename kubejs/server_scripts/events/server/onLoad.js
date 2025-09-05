74967d9b5a49b2432e4650d4cf4497fb7b51f5c1
Desynq <notdesynq@gmail.com>
2025-09-04 20:57:45 -0400

    Removed test code

--- kubejs/server_scripts/events/server/onLoad.js
+++ kubejs/server_scripts/events/server/onLoad.js
@@ -9,10 +9,6 @@ const OnLoad = {};
  * @param {$MinecraftServer_} server 
  */
 OnLoad.run = function (server) {
-	const customBossEvents = server.customBossEvents;
-	if (customBossEvents.get(BOSSBAR_ID_DESYNQ) == null) {
-		server.runCommandSilent(`bossbar add ${BOSSBAR_ID_DESYNQ} ""`);
-	}
 }
 
 
