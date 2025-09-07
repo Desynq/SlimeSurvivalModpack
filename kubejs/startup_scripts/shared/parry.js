287b78de9b648d2aa3897041e38ef4552c70271c
Desynq <notdesynq@gmail.com>
2025-09-07 09:52:42 -0400

    Moved weak knees and parry functionality to startup for client-server sync

--- kubejs/startup_scripts/shared/parry.js
+++ kubejs/startup_scripts/shared/parry.js
@@ -1,37 +1,11 @@
-/** @type {typeof import("net.minecraft.world.entity.LivingEntity").$LivingEntity } */
-let $LivingEntity  = Java.loadClass("net.minecraft.world.entity.LivingEntity")
-/** @type {typeof import("net.minecraft.world.entity.projectile.AbstractArrow").$AbstractArrow } */
-let $AbstractArrow  = Java.loadClass("net.minecraft.world.entity.projectile.AbstractArrow")
-/** @type {typeof import("net.neoforged.neoforge.event.entity.player.AttackEntityEvent").$AttackEntityEvent } */
-let $AttackEntityEvent  = Java.loadClass("net.neoforged.neoforge.event.entity.player.AttackEntityEvent")
 
-/** @type {typeof import("fuzs.swordblockingmechanics.handler.SwordBlockingHandler").$SwordBlockingHandler} */
-const $SwordBlockingHandler = Java.loadClass("fuzs.swordblockingmechanics.handler.SwordBlockingHandler");
-
-/** @type {typeof import("fuzs.swordblockingmechanics.SwordBlockingHandler").$SwordBlockingMechanics} */
-const $SwordBlockingMechanics = Java.loadClass("fuzs.swordblockingmechanics.SwordBlockingMechanics");
-
-/** @type {typeof import("fuzs.swordblockingmechanics.config.ServerConfig").$SBMServerConfig} */
-const $SBMServerConfig = Java.loadClass("fuzs.swordblockingmechanics.config.ServerConfig");
-
-/** @type {typeof import("net.minecraft.tags.DamageTypeTags").$DamageTypeTags} */
-const $DamageTypeTags = Java.loadClass("net.minecraft.tags.DamageTypeTags");
-
-
-NativeEvents.onEvent($EventPriority.HIGH, $LivingIncomingDamageEvent, event => {
-	try {
-		detectParryEvent(event)
-	}
-	catch (error) {
-		console.log(error);
-	}
-});
+const Parry = {};
 
 /**
  * @param {import("net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent").$LivingIncomingDamageEvent$$Original} event
  */
-function detectParryEvent(event) {
-	const player = event.entity instanceof $ServerPlayer ? event.entity : null;
+Parry.onLivingIncomingDamage = function(event) {
+	const player = event.entity instanceof $Player ? event.entity : null;
 	if (player == null) {
 		return;
 	}
@@ -41,25 +15,25 @@ function detectParryEvent(event) {
 	if (event.amount <= 0) {
 		return;
 	}
-	if (!canBlockDamageSource(player, event.source)) {
-		playerParryWhiffed(player, event);
+	if (!Parry.canBlockDamageSource(player, event.source)) {
+		Parry.playerParryWhiffed(player, event);
 		return;
 	}
 	const isParryActive = $SwordBlockingHandler.getParryStrengthScale(player) > 0;
 	const isDeflectable = $SwordBlockingMechanics.CONFIG.get($SBMServerConfig).deflectProjectiles && event.source.is($DamageTypeTags.IS_PROJECTILE);
 	if (!isParryActive && !isDeflectable) {
-		playerParryWhiffed(player, event);
+		Parry.playerParryWhiffed(player, event);
 		return;
 	}
 
-	playerParried(player, event);
+	Parry.playerParried(player, event);
 }
 
 /**
- * @param {import("net.minecraft.server.level.ServerPlayer").$ServerPlayer$$Original} player
+ * @param {import("net.minecraft.world.entity.player.Player").$Player$$Original} player
  * @param {import("net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent").$LivingIncomingDamageEvent$$Original} damageEvent
  */
-function playerParried(player, damageEvent) {
+Parry.playerParried = function(player, damageEvent) {
 	const attacker = damageEvent.source.actual instanceof $LivingEntity ? damageEvent.source.actual : null;
 	if (attacker == null) {
 		return;
@@ -72,24 +46,25 @@ function playerParried(player, damageEvent) {
 }
 
 /**
- * @param {import("net.minecraft.server.level.ServerPlayer").$ServerPlayer$$Original} player
+ * @param {import("net.minecraft.world.entity.player.Player").$Player$$Original} player
  * @param {import("net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent").$LivingIncomingDamageEvent$$Original} damageEvent
  */
-function playerParryWhiffed(player, damageEvent) {
+Parry.playerParryWhiffed = function(player, damageEvent) {
 	const miningFatigueEffect = new $MobEffectInstance("minecraft:mining_fatigue", 100, 1, false, true, true);
 	const slownessEffect = new $MobEffectInstance("minecraft:slowness", 100, 1, false, true, true);
 	player.addEffect(miningFatigueEffect);
 	player.addEffect(slownessEffect);
+	player.resetAttackStrengthTicker();
 }
 
 
 /**
  * 
- * @param {import("net.minecraft.server.level.ServerPlayer").$ServerPlayer$$Original} player 
+ * @param {import("net.minecraft.world.entity.player.Player").$Player$$Original} player 
  * @param {import("net.minecraft.world.damagesource.DamageSource").$DamageSource$$Original} source 
  * @returns 
  */
-function canBlockDamageSource(player, source) {
+Parry.canBlockDamageSource = function(player, source) {
 	const entity = source.actual;
 	const arrow = entity instanceof $AbstractArrow ? entity : null;
 	if (arrow) {
@@ -114,3 +89,7 @@ function canBlockDamageSource(player, source) {
 	const viewVector = player.getViewVector(1.0);
 	return position.dot(viewVector) <= arcDotThreshold;
 }
+
+
+global.Parry = Parry;
+NativeEvents.onEvent($EventPriority.HIGH, $LivingIncomingDamageEvent, event => global.Parry.onLivingIncomingDamage(event));
