2e5fac7e7961ccb06b98bc3ca842629291136963
Desynq <notdesynq@gmail.com>
2025-09-09 23:18:37 -0400

    Added more typings

--- kubejs/server_scripts/types/net.minecraft.world.entity.d.ts
+++ kubejs/server_scripts/types/net.minecraft.world.entity.d.ts
@@ -3,3 +3,7 @@ type Player = import("net.minecraft.world.entity.player.Player").$Player$$Origin
 type MinecraftServer = import("net.minecraft.server.MinecraftServer").$MinecraftServer$$Original;
 
 type ItemEntity = import("net.minecraft.world.entity.item.ItemEntity").$ItemEntity$$Original;
+
+type LivingEntity = import("net.minecraft.world.entity.LivingEntity").$LivingEntity$$Original;
+
+type Entity = import("net.minecraft.world.entity.Entity").$Entity$$Original;
