3c8d238330e5b869dd4f9e763f05650b1a47af0e
Desynq <notdesynq@gmail.com>
2025-09-09 22:17:16 -0400

    Made entities spawned from spawners and trial spawners not despawn normally

--- kubejs/server_scripts/entities/persistentCustomSpawns.js
+++ kubejs/server_scripts/entities/persistentCustomSpawns.js
@@ -1,11 +1,13 @@
 /** @type {typeof import("net.minecraft.world.entity.MobSpawnType").$MobSpawnType } */
 let $MobSpawnType = Java.loadClass("net.minecraft.world.entity.MobSpawnType")
 
+const PERSISTENT_SPAWN_TYPES = [$MobSpawnType.SPAWN_EGG, $MobSpawnType.SPAWNER, $MobSpawnType.TRIAL_SPAWNER];
+
 EntityEvents.checkSpawn(event => {
 	const server = event.server;
 	const entity = event.entity;
 	$TaskScheduler.scheduleTask(server, 1, () => {
-		if (event.type !== $MobSpawnType.SPAWN_EGG) {
+		if (PERSISTENT_SPAWN_TYPES.indexOf(event.type) === -1) {
 			return;
 		}
 		CommandHelper.runCommandSilent(event.server, `data modify entity ${entity.username} PersistenceRequired set value true`);
