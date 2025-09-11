c0681feb51e57e0ea20c965812023a68fa7b2afb
Desynq <notdesynq@gmail.com>
2025-09-11 13:29:49 -0400

    Made default race skills get unlocked for the player whenever they rejoin or the server is reloaded.

---
+++ kubejs/server_scripts/races/applyDefaultSkills.js
@@ -0,0 +1,9 @@
+/** @type {typeof import("net.neoforged.neoforge.event.OnDatapackSyncEvent").$OnDatapackSyncEvent } */
+let $OnDatapackSyncEvent = Java.loadClass("net.neoforged.neoforge.event.OnDatapackSyncEvent")
+
+NativeEvents.onEvent($OnDatapackSyncEvent, event => {
+	event.getPlayerList().getPlayers().forEach(player => {
+		// @ts-ignore
+		PlayerRaceSkillHelper.unlockDefaultRaceSkills(player, PlayerRaceHelper.getRace(player));
+	});
+});
