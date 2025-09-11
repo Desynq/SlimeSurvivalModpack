38a9014eb1b46507b3351bc6e7956cdc24d99873
Desynq <notdesynq@gmail.com>
2025-09-11 16:46:22 -0400

    Refactored blockPosHelper to BlockPosHelper

--- kubejs/server_scripts/helpers/blockPosHelper.js
+++ kubejs/server_scripts/helpers/blockPosHelper.js
@@ -1,9 +1,9 @@
 const BlockPosHelper = {}
 
 /**
- * 
- * @param {$BlockPos_} pos 
+ * Converts a BlockPos object to a JavaScript number array so that's easier to modify its data.
+ * @param {import("net.minecraft.core.BlockPos").$BlockPos$$Original} pos 
  */
-BlockPosHelper.toIntArray = function (pos) {
+BlockPosHelper.toIntArray = function(pos) {
 	return [pos.getX(), pos.getY(), pos.getZ()];
 }
