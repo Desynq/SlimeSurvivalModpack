bf215c866b5a41c4f523d5fea4ecd21e65264f15
Desynq <notdesynq@gmail.com>
2025-09-14 13:00:15 -0400

    Fixed inheritance problems with ServerPlayer, Player, and LivingEntity

---
+++ kubejs/server_scripts/types/inheritance.d.ts
@@ -0,0 +1,7 @@
+declare module "net.minecraft.server.level.ServerPlayer" {
+	declare class $ServerPlayer extends $Player { }
+}
+
+declare module "net.minecraft.world.entity.player.Player" {
+	declare class $Player extends $LivingEntity { }
+}
