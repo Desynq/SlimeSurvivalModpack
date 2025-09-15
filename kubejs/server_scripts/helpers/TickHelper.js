f5bd5c8b61227e8b420ce347405865cfa3a0cecd
Desynq <notdesynq@gmail.com>
2025-09-14 23:06:18 -0400

    Added new TickHelper.timestamp function to help with cooldowns and intervals

--- kubejs/server_scripts/helpers/TickHelper.js
+++ kubejs/server_scripts/helpers/TickHelper.js
@@ -18,3 +18,23 @@ TickHelper.getTickRate = function(server) {
 TickHelper.getGameTime = function(server) {
 	return server.overworld().levelData.getGameTime();
 }
+
+
+/**
+ * 
+ * @param {Entity} entity 
+ * @param {string} id
+ * @param {long} interval
+ * @returns {boolean} true if timestamp was successfully updated after interval passed
+ */
+TickHelper.timestamp = function(entity, id, interval) {
+	const currentTimestamp = TickHelper.getGameTime(entity.server);
+	const oldTimestamp = entity.persistentData.getLong(id);
+	if (currentTimestamp - oldTimestamp < interval) {
+		return false;
+	}
+	else {
+		entity.persistentData.putLong(id, currentTimestamp);
+		return true;
+	}
+}
