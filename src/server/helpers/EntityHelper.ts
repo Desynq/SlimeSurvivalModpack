efb47ec2d3fe53e9a092caa65b895d2b60d85358
Desynq <notdesynq@gmail.com>
2025-10-02 23:01:23 -0400

    Added helper function for teleporting an entity randomly within a circle of a Vec3 with radius.

--- src/server/helpers/EntityHelper.ts
+++ src/server/helpers/EntityHelper.ts
@@ -18,4 +18,55 @@ namespace EntityHelper {
 	export function isType(entity: Entity_, type: string): boolean {
 		return $BuiltInRegistries.ENTITY_TYPE.getKey(entity.getEntityType()).toString() === type;
 	}
+
+	export function teleportRandCircle(entity: Entity_, center: Vec3_, radius: double): void {
+		let nX = center.x();
+		let nY = center.y();
+		let nZ = center.z();
+
+		let found = false;
+
+		for (let i = 0; i < Math.max(1, Math.floor(radius)); i++) {
+			const angle = Math.random() * (2 * Math.PI);
+			const dist = Math.sqrt(Math.random()) * radius;
+
+			const testX = center.x() + Math.cos(angle) * dist;
+			const testZ = center.z() + Math.sin(angle) * dist;
+			const targetY = center.y();
+
+			const from = new Vec3d(entity.x, entity.y + entity.eyeHeight, entity.z);
+			const to = new Vec3d(testX, targetY + entity.eyeHeight, testZ);
+
+			const hit = entity.level.clip(new $ClipContext(
+				from as any,
+				to as any,
+				"collider",
+				"none",
+				entity
+			));
+
+			if (hit.type === $HitResult$Type.MISS) {
+				nX = Math.floor(testX) + 0.5;
+				nY = Math.floor(targetY);
+				nZ = Math.floor(testZ) + 0.5;
+				found = true;
+				break;
+			}
+		}
+
+		if (!found) {
+			const angle = Math.random() * (2 * Math.PI);
+			const dist = Math.sqrt(Math.random()) * radius;
+			const testX = center.x() + Math.cos(angle) * dist;
+			const testZ = center.z() + Math.sin(angle) * dist;
+
+			nX = Math.floor(testX) + 0.5;
+			nZ = Math.floor(testZ) + 0.5;
+
+			const surfaceY = entity.level.getHeight("motion_blocking_no_leaves", Math.floor(nX), Math.floor(nZ));
+			nY = Math.floor(surfaceY + 1);
+		}
+
+		entity.teleportTo(nX, nY, nZ);
+	}
 }
