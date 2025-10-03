93a9106ea02632d79c76775a9d3159b12821ddb3
Desynq <notdesynq@gmail.com>
2025-10-02 21:02:36 -0400

    Added more repair recipes for armor

--- src/server/events/recipe/repair.ts
+++ src/server/events/recipe/repair.ts
@@ -3,14 +3,16 @@
 
 namespace RepairLeather {
 
-	function repair(stack: ItemStack_, materialCost: integer) {
+	function repair(stack: ItemStack_, repairPercentage: float) {
+		if (repairPercentage > 1) throw new Error(`Repair percentage of ${repairPercentage} is > 1 for item: ${stack.id}`);
 		const currentDamage = stack.getDamageValue();
 		const maxDamage = stack.getMaxDamage();
-		const newDamage = Math.max(0, Math.floor(currentDamage - maxDamage / materialCost));
+		const repairAmount = Math.ceil(maxDamage * repairPercentage);
+		const newDamage = Math.max(0, currentDamage - repairAmount);
 		stack.setDamage(newDamage);
 	}
 
-	function createRepairRecipe(id: string, materialId: string, materialCost: integer) {
+	function createRepairRecipe(id: string, materialId: string, repairPercentage: float) {
 		const modifyResultId = id.split(":")[1] + "_repair";
 
 		ServerEvents.recipes(event => {
@@ -21,18 +23,30 @@ namespace RepairLeather {
 		ServerEvents.modifyRecipeResult(modifyResultId, event => {
 			// @ts-ignore
 			const stack = event.grid.find(Ingredient.of(id));
-			repair(stack, materialCost);
+			repair(stack, repairPercentage);
 			event.success(stack);
 		});
 	}
 
-	function createArmorRepairRecipe(partId: string, materialId: string, materialCost: [integer, integer, integer, integer]) {
+	const VANILLA_COSTS = [5, 8, 7, 4];
+	const CHESTPLATE_COST = 8;
+
+	function getRepairFraction(pieceCost: integer, chestplateRepairFraction: float): float {
+		return (CHESTPLATE_COST / pieceCost) * chestplateRepairFraction;
+	}
+
+	function createArmorRepairRecipe(partId: string, materialId: string, chestplateRepairFraction: float) {
+		let pieceCost = [5, 8, 7, 4];
+		pieceCost.map(x => Math.trunc(x * (1 - chestplateRepairFraction)));
 		const parts = ["_helmet", "_chestplate", "_leggings", "_boots"];
 		for (let i = 0; i < parts.length; i++) {
-			createRepairRecipe(partId + parts[i], materialId, materialCost[i]);
+			const scaledPercentage = getRepairFraction(pieceCost[i], chestplateRepairFraction);
+			createRepairRecipe(partId + parts[i], materialId, scaledPercentage);
 		}
 	}
 
-	createArmorRepairRecipe("minecraft:leather", "minecraft:leather", [3, 6, 6, 3]);
-	createArmorRepairRecipe("minecraft:netherite", "minecraft:netherite_scrap", [2, 4, 4, 2]);
+	createArmorRepairRecipe("minecraft:leather", "minecraft:leather", 1 / 4);
+	createArmorRepairRecipe("minecraft:iron", "minecraft:iron_ingot", 1 / 8);
+	createArmorRepairRecipe("minecraft:diamond", "minecraft:diamond", 1 / 4);
+	createArmorRepairRecipe("minecraft:netherite", "minecraft:netherite_scrap", 1 / 3);
 }
