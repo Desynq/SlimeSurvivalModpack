e64a0859810ba6aa6e337d03aeb06d9573ccfd7c
Desynq <notdesynq@gmail.com>
2025-10-11 19:06:35 -0400

    Made redstone buyable

--- src/server/economy/marketable-items/miscMinerals.js
+++ src/server/economy/marketable-items/miscMinerals.js
@@ -1,6 +1,7 @@
 //priority: 100
 new MarketableItem("redstone", "minecraft:redstone")
 	.setSellPrice(0.10)
+	.setBuyPrice(5.00)
 	.setCompoundingRate(0.5)
 	.setCompoundingPeriod(2000)
 	.register();
