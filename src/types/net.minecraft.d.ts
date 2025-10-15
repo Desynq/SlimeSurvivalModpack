c09f8b0ff4204e54b07f189a31875dba03809ef0
Desynq <notdesynq@gmail.com>
2025-10-15 03:01:46 -0400

    Added more type imports

--- src/types/net.minecraft.d.ts
+++ src/types/net.minecraft.d.ts
@@ -3,3 +3,5 @@ type CompoundTag_ = import("net.minecraft.nbt.CompoundTag").$CompoundTag$$Origin
 type Phantom_ = import("net.minecraft.world.entity.monster.Phantom").$Phantom;
 
 type ShulkerBullet_ = import("net.minecraft.world.entity.projectile.ShulkerBullet").$ShulkerBullet;
+
+type CustomData_ = import("net.minecraft.world.item.component.CustomData").$CustomData;
