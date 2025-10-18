c2a4f59e0ea3bc304fa6ddead975c04114fe41f6
Desynq <notdesynq@gmail.com>
2025-10-18 19:13:35 -0400

    Made explosions not break blocks in non-overworld dimensions

--- src/server/events/dimensions/noExplosionBlockDamage.ts
+++ src/server/events/dimensions/noExplosionBlockDamage.ts
@@ -3,36 +3,19 @@ let $TntBlock: typeof import("net.minecraft.world.level.block.TntBlock").$TntBlo
 let $ExplosionEvent$Detonate: typeof import("net.neoforged.neoforge.event.level.ExplosionEvent$Detonate").$ExplosionEvent$Detonate = Java.loadClass("net.neoforged.neoforge.event.level.ExplosionEvent$Detonate");
 
 namespace NoExplosionBlockDamage {
-	function inBlacklistedDimension(level: Level_) {
-		switch (level.getDimension().toString()) {
-			case "slimesurvival:wipe":
+	function inWhitelistedDimension(level: ServerLevel_) {
+		const dimension = level.getDimension().toString();
+		switch (dimension) {
+			case "minecraft:overworld":
 				return true;
 			default:
 				return false;
 		}
 	}
 
-	function pruneAffectedBlocks(
-		affected: import("java.util.List").$List$$Original<import("net.minecraft.core.BlockPos").$BlockPos$$Original>,
-		level: Level_
-	) {
-		const keepers: import("net.minecraft.core.BlockPos").$BlockPos$$Original[] = [];
-		affected.forEach(pos => {
-			// @ts-ignore
-			const state = level.getBlockState(pos);
-			// @ts-ignore
-			if (state.getBlock() instanceof $TntBlock && state.getValue($TntBlock.UNSTABLE)) {
-				keepers.push(pos);
-			}
-		});
-
-		affected.clear();
-		affected.addAll(keepers);
-	}
-
 	function pruneAffectedBlocksOld(
 		affected: import("java.util.List").$List$$Original<import("net.minecraft.core.BlockPos").$BlockPos$$Original>,
-		level: Level_
+		level: ServerLevel_
 	) {
 		affected.removeIf(pos => {
 			// @ts-ignore
@@ -46,10 +29,10 @@ namespace NoExplosionBlockDamage {
 	}
 
 	NativeEvents.onEvent($ExplosionEvent$Detonate, event => {
-		if (!inBlacklistedDimension(event.getLevel())) return;
+		const blocks = event.getAffectedBlocks();
+		const level = event.getLevel() as ServerLevel_;
+		if (inWhitelistedDimension(level)) return;
 
-		// shit broke
-		// pruneAffectedBlocks(event.getAffectedBlocks(), event.getLevel());
-		pruneAffectedBlocksOld(event.getAffectedBlocks(), event.getLevel());
+		pruneAffectedBlocksOld(blocks, level);
 	});
 }
