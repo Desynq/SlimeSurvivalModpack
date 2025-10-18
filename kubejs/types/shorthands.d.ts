381306257c77eaeb51386cd6cd90ffac3c4f2c35
Desynq <notdesynq@gmail.com>
2025-10-18 19:16:49 -0400

    More type inheritance fixing

--- kubejs/types/shorthands.d.ts
+++ kubejs/types/shorthands.d.ts
@@ -1,6 +1,6 @@
 type RecipeInput_ = import("net.minecraft.world.item.crafting.RecipeInput").$RecipeInput$$Original;
 
-type ServerLevel_ = import("net.minecraft.server.level.ServerLevel").$ServerLevel$$Original;
+type ServerLevel_ = Level_ & import("net.minecraft.server.level.ServerLevel").$ServerLevel$$Original;
 
 type Vec3_ = import("net.minecraft.world.phys.Vec3").$Vec3$$Original;
 
