0e703f15424fe0770779dc288f4cd953dbaad630
Desynq <notdesynq@gmail.com>
2025-10-19 10:07:35 -0400

    Added skills draft for Sculker (still not visible in-game)

---
+++ src/server/datagen/skill-definitions/sculker/main.ts
@@ -0,0 +1,73 @@
+//priority: 101
+
+class SculkerSkills {
+	public static readonly CATEGORY_ID = "slimesurvival:sculker_race";
+
+	public static readonly definitionsJson: Object = {};
+	public static readonly skills: Skill[] = [];
+
+	private static createSkill(id: string, decorate: (def: SkillDefinition) => void): Skill {
+		const def = new SkillDefinition(this.CATEGORY_ID, id);
+		decorate(def);
+		return def.serializeIntoSkill(this.definitionsJson).register(this.skills);
+	}
+
+	public static readonly BLIND = this.createSkill("blind", def => def
+		.effectIcon("minecraft:blindness")
+		.addDescription({
+			"text": "Who's there?",
+			"color": "dark_aqua",
+			"italic": true
+		})
+		.addDescription({
+			"text": "\n\nYou cannot see living entities who aren't pinged.",
+			"color": "dark_aqua"
+		})
+		.size(1.25)
+		.rootSkill()
+	);
+
+	public static readonly ECHOLOCATION = this.createSkill("echolocation", def => def
+		.effectIcon("slimesurvival:pinged")
+		.addDescription({
+			"text": "I hear you...",
+			"color": "dark_aqua",
+			"italic": true
+		})
+		.addDescription({
+			"text": "\n\nLiving entities that produce a sound event within 32 blocks of you get pinged for 1 second.",
+			"color": "dark_aqua"
+		})
+		.size(1.25)
+		.rootSkill()
+	);
+
+	public static readonly CHITINOUS = this.createSkill("chitinous", def => def
+		.itemIcon("minecraft:popped_chorus_fruit")
+		.addDescription({
+			"text": "That tickles...",
+			"color": "dark_aqua",
+			"italic": true
+		})
+		.addDescription({
+			"text": "\n\nArmor toughness reduces damage by a flat amount rather than by percentage.",
+			"color": "dark_aqua"
+		})
+		.addDescription({
+			"text": "\nArmor reduces damage by a rational percentage.",
+			"color": "dark_aqua"
+		})
+		.addDescription({
+			"text": "\n\nMath.max(1, damage - toughness * toughnessFactor) * (armorFactor / (armor + armorFactor))",
+			"color": "dark_aqua",
+			"italic": true
+		})
+		.addDescription({
+			"text": "\ntoughnessFactor = 0.5; armorFactor = 20",
+			"color": "dark_aqua",
+			"bold": true
+		})
+		.size(1.25)
+		.rootSkill()
+	);
+}
