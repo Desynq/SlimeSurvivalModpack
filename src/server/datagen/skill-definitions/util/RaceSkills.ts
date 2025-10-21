9485494a085934b54b4395cde14c375647964d1b
Desynq <notdesynq@gmail.com>
2025-10-21 14:16:11 -0400

    Added tiered skills factory method to SkillManager

--- src/server/datagen/skill-definitions/util/RaceSkills.ts
+++ src/server/datagen/skill-definitions/util/RaceSkills.ts
@@ -18,6 +18,17 @@ abstract class SkillManager {
 		return def.serializeIntoSkill(this.definitionsJson).register(this.skills);
 	}
 
+	protected createTieredSkills(baseId: string, tiers: integer, decorate: (def: SkillDefinition, tier: integer, prevSkills: Skill[]) => void): Skill[] {
+		const skills: Skill[] = [];
+		for (let tier = 1; tier <= tiers; tier++) {
+			const def = new SkillDefinition(this.categoryId, `${baseId}_${tier}`);
+			decorate(def, tier, skills);
+			const skill = def.serializeIntoSkill(this.definitionsJson).register(this.skills);
+			skills.push(skill);
+		}
+		return skills;
+	}
+
 	public register(): this {
 		SkillManager.INSTANCES.push(this);
 		return this;
