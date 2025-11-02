00c8d821bc3a706df6be5c53220d3c9f1f0434f4
Desynq <notdesynq@gmail.com>
2025-11-02 13:19:13 -0500

    Simplified adventure effect logic to remove edge case bugs

--- src/server/events/player/adventure.ts
+++ src/server/events/player/adventure.ts
@@ -3,39 +3,16 @@
 
 namespace AdventureHandler {
 
-	NativeEvents.onEvent($MobEffectEvent$Remove, event => {
-		const entity = event.entity;
-		const effect = event.effect;
-		const flag = entity instanceof $ServerPlayer && effect.is("slimesurvival:adventure");
-		if (!flag) return;
+	PlayerEvents.tick(event => {
+		const player = event.player as ServerPlayer_;
 
-		const player = entity;
-		if (!PlayerHelper.isAdventure(player)) return;
+		const hasAdventureEffect = player.hasEffect($SlimeSurvivalMobEffects.ADVENTURE.getDelegate());
 
-		player.setGameMode("survival");
-	});
-
-	NativeEvents.onEvent($MobEffectEvent$Expired, event => {
-		const entity = event.entity;
-		const effect = event.effectInstance;
-		const flag = entity instanceof $ServerPlayer && effect.is($SlimeSurvivalMobEffects.ADVENTURE.getDelegate());
-		if (!flag) return;
-
-		const player = entity;
-		if (!PlayerHelper.isAdventure(player)) return;
-
-		player.setGameMode("survival");
-	});
-
-	NativeEvents.onEvent($MobEffectEvent$Added, event => {
-		const entity = event.entity;
-		const effect = event.effectInstance;
-		const flag = entity instanceof $ServerPlayer && effect.is($SlimeSurvivalMobEffects.ADVENTURE.getDelegate());
-		if (!flag) return;
-
-		const player = entity;
-		if (!PlayerHelper.isSurvival(player)) return;
-
-		player.setGameMode("adventure");
+		if (hasAdventureEffect && PlayerHelper.isSurvival(player)) {
+			player.setGameMode("adventure");
+		}
+		else if (!hasAdventureEffect && PlayerHelper.isAdventure(player)) {
+			player.setGameMode("survival");
+		}
 	});
 }
