fd1489fb91a1e67bc50b9416d28a24c02a22da30
Desynq <notdesynq@gmail.com>
2025-11-02 13:18:41 -0500

    Refactored `raceCommand.ts` and added `/race set <player> <race> <points>`

--- src/server/races/raceCommand.ts
+++ src/server/races/raceCommand.ts
@@ -1,10 +1,102 @@
 namespace RaceCommand {
 
+	ServerEvents.commandRegistry(event => {
+		const { commands: Commands, arguments: Arguments } = event;
+
+		const rootCmd = Commands.literal("race");
+
+		const chooseCmd = Commands.literal("choose") // @ts-ignore
+			.then(Commands.argument("race", Arguments.STRING.create(event))
+				.suggests((context, builder) => suggestRace(builder))
+				.executes(context => {
+					try {
+						const player = context.source.getPlayer();
+						const race = Races.fromId(Arguments.STRING.getResult(context, "race"));
+						if (race === undefined) return 1;
+
+						const result = PlayerRaceHelper.chooseRace(player, race);
+						tellRaceSwitchResult(player, result, race);
+					}
+					catch (error) {
+						if (error instanceof Error) {
+							console.log(error.message + "\n" + error.stack);
+						}
+					}
+					return 1;
+				})
+			);
+
+		const setCmd = Commands.literal("set")
+			.requires(executor => executor.hasPermission(2)) // @ts-ignore
+			.then(Commands.argument("player", Arguments.PLAYER.create(event)) // @ts-ignore
+				.then(Commands.argument("race", Arguments.STRING.create(event))
+					.suggests((context, builder) => suggestRace(builder))
+					.executes(context => {
+						const player = $Arguments.PLAYER.getResult(context, "player");
+						const raceInput = $Arguments.STRING.getResult(context, "race");
+						return setRace(player, raceInput, context);
+					}) // @ts-ignore
+					.then(Commands.argument("amount", Arguments.INTEGER.create(event))
+						.executes(context => {
+							const player = $Arguments.PLAYER.getResult(context, "player");
+							const raceInput = $Arguments.STRING.getResult(context, "race");
+							const setRaceResult = setRace(player, raceInput, context);
+							if (setRaceResult === 0) return 0;
+
+							const amount = $Arguments.INTEGER.getResult(context, "amount");
+							return giveRacePoints(player, amount, context);
+						})
+					)
+				)
+			);
+
+		const pointsCmd = Commands.literal("points")
+			.requires(context => context.getPlayer().hasPermissions(2)) // @ts-ignore
+			.then(Commands.literal("add") // @ts-ignore
+				.then(Commands.argument("player", Arguments.PLAYER.create(event)) // @ts-ignore
+					.then(Commands.argument("amount", Arguments.INTEGER.create(event))
+						.executes(context => {
+							const player = $Arguments.PLAYER.getResult(context, "player");
+							const amount = $Arguments.INTEGER.getResult(context, "amount");
+							return giveRacePoints(player, amount, context);
+						})
+					)
+				)
+			);
+
+		rootCmd
+			.then(chooseCmd as any)
+			.then(setCmd as any)
+			.then(pointsCmd as any);
+
+		event.register(rootCmd);
+	});
+
+
 	function suggestRace(builder: SuggestionsBuilder_) {
-		for (let race of Races.getRaces()) {
+		for (const race of Races.getRaces()) {
 			builder.suggest(race.getRaceId());
 		}
 		return builder.buildFuture();
+	}
+
+	function setRace(player: ServerPlayer_, raceInput: string, context?: CommandExecutionContext): integer {
+		const race = Races.fromId(raceInput);
+		if (race === undefined) return 0;
+
+		const result = PlayerRaceHelper.chooseRace(player, race, true);
+		tellRaceSwitchResult(player, result, race);
+
+		return result.code === "SUCCESS" ? 1 : 0;
+	}
+
+	function giveRacePoints(player: ServerPlayer_, amount: integer, context?: CommandExecutionContext): integer {
+		PlayerRaceSkillHelper.addSkillPoint(player, amount);
+		if (context) {
+			const raceId = PlayerRaceHelper.getRace(player).getRaceId();
+			context.source.sendSuccess(`Gave ${player.username} ${amount} ${raceId} skill points.`, true);
+		}
+		return 1;
 	}
 
 	function tellRaceSwitchResult(player: ServerPlayer_, result: RaceSwitchResult, race: Race) {
@@ -20,70 +112,4 @@ namespace RaceCommand {
 				break;
 		}
 	}
-
-	ServerEvents.commandRegistry(event => {
-		const { commands: Commands, arguments: Arguments } = event;
-
-		const nodes = {};
-		nodes["/race"] = Commands.literal("race");
-
-		nodes["/race choose"] = Commands.literal("choose") // @ts-ignore
-			.then(Commands.argument("race", Arguments.STRING.create(event))
-				.suggests((context, builder) => suggestRace(builder))
-				.executes(context => {
-					try {
-						let player = context.source.getPlayer();
-						let race = Races.fromId(Arguments.STRING.getResult(context, "race"));
-						if (race === undefined) return 1;
-						let result = PlayerRaceHelper.chooseRace(player, race);
-						tellRaceSwitchResult(player, result, race);
-					}
-					catch (error) {
-						if (error instanceof Error) {
-							console.log(error.message + "\n" + error.stack);
-						}
-					}
-					return 1;
-				})
-			);
-
-		nodes["/race set"] = Commands.literal("set")
-			.requires(executor => executor.hasPermission(2)) // @ts-ignore
-			.then(Commands.argument("player", Arguments.PLAYER.create(event)) // @ts-ignore
-				.then(Commands.argument("race", Arguments.STRING.create(event))
-					.suggests((context, builder) => suggestRace(builder))
-					.executes(context => {
-						let player = Arguments.PLAYER.getResult(context, "player");
-						let race = Races.fromId(Arguments.STRING.getResult(context, "race"));
-						if (race === undefined) return 1;
-						let result = PlayerRaceHelper.chooseRace(player, race, true);
-						tellRaceSwitchResult(player, result, race);
-						return 1;
-					})
-				)
-			);
-
-		nodes["/race points"] = Commands.literal("points")
-			.requires(context => context.getPlayer().hasPermissions(2)) // @ts-ignore
-			.then(Commands.literal("add") // @ts-ignore
-				.then(Commands.argument("player", Arguments.PLAYER.create(event)) // @ts-ignore
-					.then(Commands.argument("amount", Arguments.INTEGER.create(event))
-						.executes(context => {
-							let player: ServerPlayer_ = Arguments.PLAYER.getResult(context, "player");
-							let amount = Arguments.INTEGER.getResult(context, "amount");
-							PlayerRaceSkillHelper.addSkillPoint(player, amount);
-							context.source.getPlayer().tell(`Gave ${player.username} ${amount} ${PlayerRaceHelper.getRace(player).getRaceId()} skill points.`);
-							return 1;
-						})
-					)
-				)
-			);
-
-		nodes["/race"]
-			.then(nodes["/race choose"])
-			.then(nodes["/race set"])
-			.then(nodes["/race points"]);
-
-		event.register(nodes["/race"]);
-	});
 }
