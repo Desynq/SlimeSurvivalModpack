83cc4cf1b483217f45a3a9f318b7e1d5a4b6f2e0
Desynq <notdesynq@gmail.com>
2025-11-07 23:31:37 -0500

    Refactor CannotMount function to use const and arrow function syntax

--- src/server/entities/manager/util/mixins/BossMixins.ts
+++ src/server/entities/manager/util/mixins/BossMixins.ts
@@ -3,13 +3,13 @@
 namespace EntityTraits {
 	type EM = AbstractConstructor<EntityManager<any>>;
 
-	export function CannotMount<TBase extends EM>(Base: TBase) {
+	export const CannotMount = <TBase extends EM>(Base: TBase) => {
 		abstract class Mixin extends Base {
 			public override onEntityMount(boss: LivingEntity_, event: EntityMountEvent_) {
 				super.onEntityMount(boss, event);
 				event.setCanceled(true);
 			}
-		};
+		}
 		return Mixin;
-	}
+	};
 }
