4eabc32615f93067e0baa602d5e4ba8eadb843da
Desynq <notdesynq@gmail.com>
2025-11-09 21:06:35 -0500

    Increase spawn rate for umvuthana raptor in slime spawn overrides

--- src/server/registry/MobSpawnOverrides.ts
+++ src/server/registry/MobSpawnOverrides.ts
@@ -4,7 +4,7 @@ namespace MobSpawnOverrides {
 
 	CustomSpawnRegistry.INSTANCE
 		.setOverride("minecraft:slime", new FixedSpawnTable.Builder()
-			.addEntry("mowziesmobs:umvuthana_raptor", 0.01)
+			.addEntry("mowziesmobs:umvuthana_raptor", 0.05)
 			.addEntry("mowziesmobs:lantern", 0.05)
 			.addCappedEntry("cataclysm:cindaria", 0.05, 8)
 			.build()
