ec02ad65e046ce497799da05b33a94069042b1cd
Desynq <notdesynq@gmail.com>
2025-11-10 19:56:23 -0500

    Refactor recipe removal logic to use a centralized array for better maintainability Remove water wheel and encased fan recipes

--- src/server/event/recipe/removeRecipes.ts
+++ src/server/event/recipe/removeRecipes.ts
@@ -1,7 +1,17 @@
 
 
-ServerEvents.recipes(event => {
-	event.remove("createdieselgenerators:crafting/hammer" as any);
+namespace RemoveRecipes {
+	const removedRecipes: string[] = [
+		"createdieselgenerators:crafting/hammer",
+		"create:crafting/materials/andesite_alloy",
+		"create:crafting/kinetics/water_wheel",
+		"create:crafting/kinetics/encased_fan",
+		"dndesires:crafting/industrial_fan"
+	];
 
-	event.remove("create:crafting/materials/andesite_alloy" as any);
-});
+	ServerEvents.recipes(event => {
+		for (const recipe of removedRecipes) {
+			event.remove(recipe as any);
+		}
+	});
+}
