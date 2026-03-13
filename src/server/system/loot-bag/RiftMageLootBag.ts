6a17758db25e2aa13ad97c394028b18d14d8cd1d
Desynq <notdesynq@gmail.com>
2026-03-12 20:04:23 -0400

    Change rift mage loot bag

--- src/server/system/loot-bag/RiftMageLootBag.ts
+++ src/server/system/loot-bag/RiftMageLootBag.ts
@@ -11,15 +11,19 @@ class RiftMageLootBag extends LootBag {
 	public override open(player: ServerPlayer_): void {
 		PlaysoundHelper.playsound(player.level, player.position(), "entity.turtle.egg_break", "master", 1, 1);
 
-		if (Math.random() < 0.1) this.giveUnique(player);
+		if (Math.random() < 0.20) this.giveUnique(player);
 		PlayerHelper.give(player, "minecraft:ender_pearl", MathHelper.randInt(8, 16));
 		PlayerHelper.give(player, "minecraft:shulker_shell", MathHelper.randInt(1, 4));
+
+		if (Math.random() < 0.25) {
+			PlayerHelper.give(player, "slimesurvival:lesser_unbreaking_tome");
+		}
 	}
 
 	private readonly UNIQUES = [
 		"twilightforest:ender_bow",
-		"simplyswords:watcher_claymore",
-		"simplyswords:watching_warglaive"
+		"simplyswords:arcanethyst",
+		"simplyswords:shadowsting"
 	];
 
 	private giveUnique(player: ServerPlayer_): void {
