41cb0c3282e37e9e5f894f46b24f8be8bc7762e1
Desynq <notdesynq@gmail.com>
2026-03-13 01:34:29 -0400

    Move typing

--- kubejs/types/net.minecraft.world.entity.d.ts
+++ kubejs/types/net.minecraft.world.entity.d.ts
@@ -1,21 +1,5 @@
 
 
-type Entity_ = import("net.minecraft.world.entity.Entity").$Entity$$Original;
-
-type ItemEntity = Entity_ & import("net.minecraft.world.entity.item.ItemEntity").$ItemEntity$$Original;
-
-
-type LivingEntity_ = Entity_ & import("net.minecraft.world.entity.LivingEntity").$LivingEntity$$Original;
-
-
-type Mob_ = LivingEntity_ & import("net.minecraft.world.entity.Mob").$Mob$$Original;
-
-type Bee_ = Mob_ & import("net.minecraft.world.entity.animal.Bee").$Bee$$Original;
-
-
-type Player_ = LivingEntity_ & import("net.minecraft.world.entity.player.Player").$Player$$Original;
-type ServerPlayer_ = Player_ & import("net.minecraft.server.level.ServerPlayer").$ServerPlayer$$Original;
-
 
 
 type MinecraftServer_ = import("net.minecraft.server.MinecraftServer").$MinecraftServer$$Original;
--- src/types/net.minecraft.d.ts
+++ src/types/net.minecraft.d.ts
@@ -5,3 +5,19 @@ type Phantom_ = import("net.minecraft.world.entity.monster.Phantom").$Phantom;
 type ShulkerBullet_ = import("net.minecraft.world.entity.projectile.ShulkerBullet").$ShulkerBullet;
 
 type CustomData_ = import("net.minecraft.world.item.component.CustomData").$CustomData;
+
+type Entity_ = import("net.minecraft.world.entity.Entity").$Entity$$Original;
+
+type ItemEntity = Entity_ & import("net.minecraft.world.entity.item.ItemEntity").$ItemEntity$$Original;
+
+
+type LivingEntity_ = Entity_ & import("net.minecraft.world.entity.LivingEntity").$LivingEntity$$Original;
+
+
+type Mob_ = LivingEntity_ & import("net.minecraft.world.entity.Mob").$Mob$$Original;
+
+type Bee_ = Mob_ & import("net.minecraft.world.entity.animal.Bee").$Bee$$Original;
+
+
+type Player_ = LivingEntity_ & import("net.minecraft.world.entity.player.Player").$Player$$Original;
+type ServerPlayer_ = Player_ & import("net.minecraft.server.level.ServerPlayer").$ServerPlayer$$Original;
