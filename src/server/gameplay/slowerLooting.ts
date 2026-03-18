62a99d83de734e957ed384d3dc335e07156e2dbe
Desynq <notdesynq@gmail.com>
2026-03-17 21:23:21 -0400

    Refactor slowerLooting.ts

--- src/server/gameplay/slowerLooting.ts
+++ src/server/gameplay/slowerLooting.ts
@@ -1,13 +1,7 @@
 
-/** @type {typeof import("net.neoforged.neoforge.event.entity.player.PlayerInteractEvent$RightClickBlock").$PlayerInteractEvent$RightClickBlock} */
-let $PlayerInteractEvent$RightClickBlock = Java.loadClass("net.neoforged.neoforge.event.entity.player.PlayerInteractEvent$RightClickBlock");
+namespace SlowerLooting {
 
-(function () {
-	/**
-	 * 
-	 * @param {ServerPlayer_} player 
-	 */
-	function isBeingTargeted(player) {
+	function isBeingTargeted(player: ServerPlayer_): boolean {
 		const aabb = player.getBoundingBox().inflate(16, 16, 16);
 		return player.level
 			// @ts-ignore
@@ -15,12 +9,7 @@ let $PlayerInteractEvent$RightClickBlock = Java.loadClass("net.neoforged.neoforg
 			.size() > 0;
 	}
 
-	/**
-	 * 
-	 * @param {ServerPlayer_} player 
-	 * @param {import("dev.latvian.mods.kubejs.level.LevelBlock").$LevelBlock$$Original} block 
-	 */
-	function canOpen(player: ServerPlayer_, block) {
+	function canOpen(player: ServerPlayer_, block: LevelBlock_): boolean {
 		if (!PlayerHelper.isSurvivalLike(player)) return true;
 
 		if (isBeingTargeted(player)) {
@@ -36,12 +25,12 @@ let $PlayerInteractEvent$RightClickBlock = Java.loadClass("net.neoforged.neoforg
 
 
 
+	// @ts-ignore
 	BlockEvents.rightClicked("minecraft:trapped_chest", event => {
-		const player = event.player;
+		const player = event.player as ServerPlayer_;
 		const block = event.block;
 
-		if (player instanceof $ServerPlayer && !canOpen(player, block)) {
-			// @ts-ignore
+		if (!canOpen(player, block)) {
 			ActionbarManager.setSimple(player, `{"color":"red","text":"Cannot open loot chest while being targeted!"}`, 20);
 			event.cancel();
 			return;
@@ -49,9 +38,10 @@ let $PlayerInteractEvent$RightClickBlock = Java.loadClass("net.neoforged.neoforg
 	});
 
 
+	// @ts-ignore
 	BlockEvents.broken("minecraft:trapped_chest", event => {
 		if (!event.player.isCreative() && !event.block.getInventory().isEmpty()) {
 			event.cancel();
 		}
 	});
-})();
+}
