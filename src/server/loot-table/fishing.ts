94d999c6c5f14b6b8ff04518cea21b5fe5cbdf33
Desynq <notdesynq@gmail.com>
2026-03-21 04:35:17 -0400

    Rebalance fishing loot table for axolotl gills and turtle scute

--- src/server/loot-table/fishing.ts
+++ src/server/loot-table/fishing.ts
@@ -3,20 +3,20 @@ LootJS.lootTables(e => {
 
 	const firstPool = e.getLootTable("minecraft:gameplay/fishing/treasure").firstPool();
 
-	firstPool.modifyItemEntry(itemEntry => {
-		const baseWeight: number = itemEntry.getWeight();
-		itemEntry.setWeight(baseWeight * 10);
+	// firstPool.modifyItemEntry(itemEntry => {
+	// 	const baseWeight: number = itemEntry.getWeight();
+	// 	itemEntry.setWeight(baseWeight * 10);
 
-		return itemEntry;
-	});
+	// 	return itemEntry;
+	// });
 
 	firstPool
 		// @ts-ignore
 		.addEntry(LootEntry.of("slimesurvival:axolotl_gills")
-			.withWeight(5)
+			.withWeight(1)
 		)
 		// @ts-ignore
 		.addEntry(LootEntry.of("minecraft:turtle_scute")
-			.withWeight(10)
+			.withWeight(1)
 		);
 });
