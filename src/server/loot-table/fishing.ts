50ce2d0538428386ca1c61c89289243455e1262c
Desynq <notdesynq@gmail.com>
2026-03-21 01:45:03 -0400

    Type fixing

--- src/server/loot-table/fishing.ts
+++ src/server/loot-table/fishing.ts
@@ -1,6 +1,4 @@
 
-
-
 LootJS.lootTables(e => {
 
 	const firstPool = e.getLootTable("minecraft:gameplay/fishing/treasure").firstPool();
@@ -13,9 +11,11 @@ LootJS.lootTables(e => {
 	});
 
 	firstPool
+		// @ts-ignore
 		.addEntry(LootEntry.of("slimesurvival:axolotl_gills")
 			.withWeight(5)
 		)
+		// @ts-ignore
 		.addEntry(LootEntry.of("minecraft:turtle_scute")
 			.withWeight(10)
 		);
