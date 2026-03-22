c1e77ebb57b0b3111fd25b23d4e5ed44c35056cc
Desynq <notdesynq@gmail.com>
2026-03-22 13:41:37 -0400

    Lower Chimera Timid skill max distance

--- src/server/system/races/chimera/constants.ts
+++ src/server/system/races/chimera/constants.ts
@@ -2,7 +2,7 @@
 
 namespace Chimera.Constants.Timid {
 
-	export const MAX_DIST = 16;
+	export const MAX_DIST = 8;
 	export const MIN_DIST = 0;
 	export const MAX_DIST_MULT = 1.0;
 	export const MIN_DIST_MULT = 0.25;
